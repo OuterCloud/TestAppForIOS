a5189b1aed0e816d0f831a790a1ab085ada178cb
tianyou.lan <tianyou.lan@jiduauto.com>
2024-07-30 12:16:13 +0800

    update

--- TestApp/ViewController.h
+++ TestApp/ViewController.h
@@ -8,7 +8,12 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
 
-
+- (IBAction)button1Tapped:(id)sender;
+- (IBAction)button2Tapped:(id)sender;
 @end
 
--- TestApp/ViewController.m
+++ TestApp/ViewController.m
@@ -7,16 +7,28 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // 设置 Label 的初始文字
+    self.myLabel.text = @"Hello, World!";
 }
 
+- (IBAction)button1Tapped:(id)sender {
+    // 当 Button1 被点击时,更新 Label 的文字
+    self.myLabel.text = @"Button1 Tapped!";
+}
+
+- (IBAction)button2Tapped:(id)sender {
+    // 当 Button2 被点击时,更新 Label 的文字
+    self.myLabel.text = @"Button2 Tapped!";
+}
+
+- (IBAction)button3Tapped:(id)sender {
+    // 当 Button3 被点击时,更新 Label 的文字
+    self.myLabel.text = @"Button3 Tapped!";
+}
 
 @end
